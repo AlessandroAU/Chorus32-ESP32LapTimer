9f746dd05f91c3cc0377ee331d78d5880a3d0466
Jye <JyeSmith@users.noreply.github.com>
2019-03-23 10:13:33 +1000

    remove AlessandroDevBoard
    Yay its dead!

--- ESP32LapTimer/HardwareConfig.h
+++ ESP32LapTimer/HardwareConfig.h
@@ -5,7 +5,6 @@ void InitHardwarePins();
 
 #define Default_Pins        // Use for all new hardware and PCBs
 //#define Old_Default_Pins
-//#define AlessandroDevBoard  // Because Alessandro was silly he has to mess around with a different pin configuration 
 
 // DO NOT CHANGE BELOW UNLESS USING CUSTOM HARDWARE
 
@@ -70,41 +69,6 @@ byte NumRecievers;
   #define CS5 25
   #define CS6 26
 
-  #define ADC1 ADC1_CHANNEL_0
-  #define ADC2 ADC1_CHANNEL_6
-  #define ADC3 ADC1_CHANNEL_7
-  #define ADC4 ADC1_CHANNEL_3
-  #define ADC5 ADC1_CHANNEL_4
-  #define ADC6 ADC1_CHANNEL_5
-  
-  #define ADC1_GPIO 36
-  #define ADC2_GPIO 34
-  #define ADC3_GPIO 35
-  #define ADC4_GPIO 39
-  #define ADC5_GPIO 32
-  #define ADC6_GPIO 33
-#endif
-
-#ifdef AlessandroDevBoard
-  #define BEEPER 23
-  
-  #define LEDs 17
-
-  #define BUTTON1 T7 // 27
-  #define BUTTON2 T4 // 13
-  
-  #define USE_HSPI
-
-  #define SCK 14
-  #define MOSI 12
-
-  #define CS1 16
-  #define CS2 2
-  #define CS3 0
-  #define CS4 15
-  #define CS5 25
-  #define CS6 26
-  
   #define ADC1 ADC1_CHANNEL_0
   #define ADC2 ADC1_CHANNEL_6
   #define ADC3 ADC1_CHANNEL_7
