7fe0a89373063b86653f114b665ffc6b4b93bbff
Aaron <apo@sto-gaming.org>
2019-05-16 21:42:36 +0100

    Updated to new pinout for revised PCB

--- ESP32LapTimer/HardwareConfig.h
+++ ESP32LapTimer/HardwareConfig.h
@@ -51,47 +51,37 @@ byte NumRecievers;
 #endif
 
 #ifdef Micro32-PCB
-  #define USE_HSPI
-  #define SCK 14
-  #define MOSI 12
+  #define USE_VSPI
   
-  #define CS1 16
-  #define CS2 6
-  #define CS3 4
-  #define CS4 8
-  #define CS5 7
-  #define CS6 11
+  #define CS1 27
+  #define CS2 26
+  #define CS3 9
+  #define CS4 13
+  #define CS5 14
+  #define CS6 12
 
   #define ADC1 ADC1_CHANNEL_0
-  #define ADC2 ADC1_CHANNEL_6
+  #define ADC2 ADC1_CHANNEL_3
   #define ADC3 ADC1_CHANNEL_7
-  #define ADC4 ADC1_CHANNEL_3
+  #define ADC4 ADC1_CHANNEL_6
   #define ADC5 ADC1_CHANNEL_4
   #define ADC6 ADC1_CHANNEL_5
   
   #define ADC1_GPIO 36
-  #define ADC2_GPIO 34
+  #define ADC2_GPIO 39
   #define ADC3_GPIO 35
-  #define ADC4_GPIO 39
+  #define ADC4_GPIO 34
   #define ADC5_GPIO 32
   #define ADC6_GPIO 33
 
-  #define BEEPER 2
-  #define ADC_SELECT 13
-  #define LED_BLUE 9
-  #define LED_RED 10
-  #define LED_GREEN 15
+  #define BEEPER 10
+  #define ADC_SELECT 25
+  #define LED_BLUE 2
+  #define LED_RED 4
+  #define LED_GREEN 19
 
-  #define ETH_RX0 25
-  #define ETH_RX1 26
-  #define ETH_CRS 27
-  #define ETH_MDC 23
-  #define ETH_TX0 19
-  #define ETH_MDIO 18
-  #define ETH_EN 17
-  #define ETH_CLK 0 //DTR
-  #define ETH_TX1 22 //SCL
-  #define ETH_TX_EN 21 //SDA
+  #define STATUS_LED 15
+
 #endif
 
 #ifdef Old_Default_Pins
