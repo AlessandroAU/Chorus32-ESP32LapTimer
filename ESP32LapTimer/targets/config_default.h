1f40687b110032f89aeaf617c056c0966dd38981
Kevin <smeatsan@gmail.com>
2019-06-25 18:03:32 +0200

    Add very simple target management

---
+++ ESP32LapTimer/targets/config_default.h
@@ -0,0 +1,29 @@
+#define BEEPER 14
+
+#define LEDs 12
+
+#define BUTTON1 T7 // 27
+#define BUTTON2 T4 // 13
+
+#define USE_VSPI
+
+#define CS1 16
+#define CS2 5
+#define CS3 4
+#define CS4 15
+#define CS5 25
+#define CS6 26
+
+#define ADC1 ADC1_CHANNEL_0
+#define ADC2 ADC1_CHANNEL_3
+#define ADC3 ADC1_CHANNEL_6
+#define ADC4 ADC1_CHANNEL_7
+#define ADC5 ADC1_CHANNEL_4
+#define ADC6 ADC1_CHANNEL_5
+
+#define ADC1_GPIO 36
+#define ADC2_GPIO 39
+#define ADC3_GPIO 34
+#define ADC4_GPIO 35
+#define ADC5_GPIO 32
+#define ADC6_GPIO 33
---
+++ ESP32LapTimer/targets/config_old.h
@@ -0,0 +1,32 @@
+#define BEEPER 23
+
+#define LEDs 17
+
+#define BUTTON1 T7 // 27
+#define BUTTON2 T4 // 13
+
+#define USE_HSPI
+
+#define SCK 14
+#define MOSI 12
+
+#define CS1 16
+#define CS2 5
+#define CS3 4
+#define CS4 15
+#define CS5 25
+#define CS6 26
+
+#define ADC1 ADC1_CHANNEL_0
+#define ADC2 ADC1_CHANNEL_6
+#define ADC3 ADC1_CHANNEL_7
+#define ADC4 ADC1_CHANNEL_3
+#define ADC5 ADC1_CHANNEL_4
+#define ADC6 ADC1_CHANNEL_5
+
+#define ADC1_GPIO 36
+#define ADC2_GPIO 34
+#define ADC3_GPIO 35
+#define ADC4_GPIO 39
+#define ADC5_GPIO 32
+#define ADC6_GPIO 33
