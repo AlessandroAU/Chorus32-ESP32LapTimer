5d319013cda9ebf1476d7db187fa0d5165822ecb
Kevin <smeatsan@gmail.com>
2019-07-19 21:14:21 +0200

    Move lap logic to Laptime (#85)
    Fix undefined behavior when requesting lap 0

--- ESP32LapTimer/Laptime.cpp
+++ ESP32LapTimer/Laptime.cpp
@@ -5,13 +5,14 @@
 #include "HardwareConfig.h"
 
 static volatile uint32_t LapTimes[MaxNumRecievers][100];
-static volatile int LapTimePtr[MaxNumRecievers] = {0, 0, 0, 0, 0, 0}; //Keep track of what lap we are up too
+static volatile int lap_counter[MaxNumRecievers] = {0, 0, 0, 0, 0, 0}; //Keep track of what lap we are up too
 
 static uint32_t MinLapTime = 5000;  //this is in millis
+static uint32_t start_time = 0;
 
 void resetLaptimes() {
   for (int i = 0; i < NumRecievers; ++i) {
-    LapTimePtr[i] = 0;
+    lap_counter[i] = 0;
   }
 }
 
@@ -20,13 +21,30 @@ uint32_t getLaptime(uint8_t receiver, uint8_t lap) {
 }
 
 uint32_t getLaptime(uint8_t receiver) {
-  return getLaptime(receiver, LapTimePtr[receiver]);
+  return getLaptime(receiver, lap_counter[receiver]);
+}
+
+uint32_t getLaptimeRel(uint8_t receiver, uint8_t lap) {
+  if(lap == 1) {
+    return getLaptime(receiver, lap) - start_time;
+  } else if(lap == 0) {
+    return 0;
+  }
+  return getLaptime(receiver, lap) - getLaptime(receiver, lap - 1);
+}
+
+uint32_t getLaptimeRelToStart(uint8_t receiver, uint8_t lap) {
+  return getLaptime(receiver, lap) - start_time;
+}
+
+uint32_t getLaptimeRel(uint8_t receiver) {
+  return getLaptimeRel(receiver, lap_counter[receiver]);
 }
 
 uint8_t addLap(uint8_t receiver, uint32_t time) {
-  LapTimePtr[receiver] = LapTimePtr[receiver] + 1;
-  LapTimes[receiver][LapTimePtr[receiver]] = time;
-  return LapTimePtr[receiver];
+  lap_counter[receiver] = lap_counter[receiver] + 1;
+  LapTimes[receiver][lap_counter[receiver]] = time;
+  return lap_counter[receiver];
 }
 
 uint32_t getMinLapTime() {
@@ -38,5 +56,10 @@ void setMinLapTime(uint32_t time) {
 }
 
 uint8_t getCurrentLap(uint8_t receiver) {
-  return LapTimePtr[receiver];
+  return lap_counter[receiver];
+}
+
+void startRaceLap() {
+  resetLaptimes();
+  start_time = millis();
 }
