17929b7b423f2fd65910bd6bdb04408c54ae8019
Kevin <smeatsan@gmail.com>
2019-07-27 11:06:05 +0200

    Replace semaphore with notify to block the ADC task Uses less RAM and is 45% faster

--- ESP32LapTimer/ESP32LapTimer.ino
+++ ESP32LapTimer/ESP32LapTimer.ino
@@ -30,21 +30,21 @@
 #define MAX_SRV_CLIENTS 5
 WiFiClient serverClients[MAX_SRV_CLIENTS];
 
-SemaphoreHandle_t adc_semaphore;
+static TaskHandle_t adc_task_handle = NULL;
 
 void IRAM_ATTR adc_read() {
-  static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   /* un-block the interrupt processing task now */
-  xSemaphoreGiveFromISR( adc_semaphore, &xHigherPriorityTaskWoken );
-  if ( xHigherPriorityTaskWoken) {
-    portYIELD_FROM_ISR(); // this wakes up sample_timer_task immediately
+  vTaskNotifyGiveFromISR(adc_task_handle, &xHigherPriorityTaskWoken);
+  if(xHigherPriorityTaskWoken) {
+    portYIELD_FROM_ISR();
   }
 }
 
 void IRAM_ATTR adc_task(void* args) {
   watchdog_add_task();
   while(42) {
-    xSemaphoreTake( adc_semaphore, portMAX_DELAY );
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     nbADCread(NULL);
     watchdog_feed();
   }
@@ -92,12 +92,11 @@ void setup() {
     setRSSIThreshold(i, EepromSettings.RSSIthresholds[i]);
   }
 
-  adc_semaphore = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(adc_task, "ADCreader", 4096, NULL, 1, &adc_task_handle, 0);
   hw_timer_t* adc_task_timer = timerBegin(0, 8, true);
   timerAttachInterrupt(adc_task_timer, &adc_read, true);
   timerAlarmWrite(adc_task_timer, 1667, true); // 6khz -> 1khz per adc channel
   timerAlarmEnable(adc_task_timer);
-  xTaskCreatePinnedToCore(adc_task, "ADCreader", 4096, NULL, 1, NULL, 0);
 
   //SelectivePowerUp();
 
