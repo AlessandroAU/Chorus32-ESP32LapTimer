d8dedf7bde1024007c6e1e482de1b30ba7582be4
Kevin <smeatsan@gmail.com>
2019-12-24 13:41:52 +0100

    Restore Arduino compatability

---
+++ ESP32LapTimer/ESP32LapTimer.ino
@@ -0,0 +1,5 @@
+/*
+We moved our build system to platformio. Take a look at the readme to see on how to use the new system.
+
+You are still able to use the Arduino IDE as you used to, but you won't be able to see the any other files. If you want to change the configuration you need to use an external editor.
+*/
